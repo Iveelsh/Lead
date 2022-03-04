b00080a08bf99089dc259b802800f4d5d2e9272e
Iveel Shijir <iveel.sh@gmail.com>
2022-03-04 17:49:27 +0800

    Added contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,12 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
-    ]
+      {
+        resolve: `gatsby-source-contentful`,
+        options: {
+          spaceId: `2y0qgi3ch4zc`,
+          accessToken: `yCZqvcqyso48MAcgAFBIxCfRybOFUQEiLI6s_9p1cwU`,
+        },
+      },
+    ],
 }
