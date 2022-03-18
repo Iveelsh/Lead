39b581e81b530ec1a647fcc7cdd49ffc46145123
Oyunbaatar <Oba@LEAP-03s-iMac.local>
2022-03-18 18:46:35 +0800

    Component border

---
+++ src/components/core/Border.js
@@ -0,0 +1,24 @@
+export const Border = (props) => {
+    const { lineWidth, radius, topWidth, bottomWidth, leftWidth, rightWidth, children, lineType, color } = props;
+
+    console.log(props)
+
+    return <div>
+        <div style={{
+            borderRight: rightWidth,
+            borderRightColor: color,
+            borderLeft: leftWidth,
+            borderLeftColor: color,
+            borderTop: topWidth,
+            borderTopColor: color,
+            borderBottom: bottomWidth,
+            borderBottomColor: color,
+            borderStyle: lineType,
+            borderRadius: radius,
+            color: color,
+            lineWidth: lineWidth,
+        }}>
+            {children}
+        </div>
+    </div>
+}
