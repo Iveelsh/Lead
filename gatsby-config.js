b3e3feb1c0a30cae222662d1c319fd40db1986a9
UnknownOne76 <ganboldgantor14@gmail.com>
2022-03-19 14:23:53 +0800

    Eslint setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,5 @@
-const path = require("path");
-const gatsbyRequiredRules = path.join(
-    process.cwd(),
-    "node_modules",
-    "gatsby",
-    "dist",
-    "utils",
-    "eslint-rules"
-);
 
-module.exports = {
-    siteMetadata: {
+module.exports = {    siteMetadata: {
         title: `new`,
         siteUrl: `https://www.yourdomain.tld`,
     },
@@ -20,16 +10,6 @@ module.exports = {
                 accessToken: `yCZqvcqyso48MAcgAFBIxCfRybOFUQEiLI6s_9p1cwU`,
             },
         },
-        {
-            resolve: "gatsby-plugin-eslint",
-            options: {
-                rulePaths: [gatsbyRequiredRules],
-                stages: ["develop"],
-                extensions: ["js", "jsx", "ts", "tsx"],
-                exclude: ["node_modules", "bower_components", ".cache", "public"],
-                // Any additional eslint-webpack-plugin options below
-            },
-        }
         `gatsby-plugin-image`,
         `gatsby-plugin-sharp`,
         `gatsby-transformer-sharp`,
